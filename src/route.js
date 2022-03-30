1f8ac613578afd8904632a52aa4567a21d7cd88a
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-30 18:53:10 -0300

    Update

--- src/route.js
+++ src/route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const questionController = require('./controllers/questionController')
 
 const route = express.Router()
 
@@ -7,6 +8,6 @@ route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 
-// route.post('/room/:room/:question/:action')
+route.post('/room/:room/:question/:action', questionController.index)
 
 module.exports = route
