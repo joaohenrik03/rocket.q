f297312c347a3d20afa0e9b6dcdf47c973b8ba1d
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-30 00:53:54 -0300

    Update js

---
+++ public/scripts/main.js
@@ -0,0 +1,14 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+    button.addEventListener('click', event => {       
+       modal.open()
+    })
+})
+
+const deleteButtons = document.querySelectorAll('.actions a.delete')
+
