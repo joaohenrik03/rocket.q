910932809f40ace045a5a4ede13ee03a9f8f53ea
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-30 18:43:10 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -33,13 +33,12 @@ function handleClick(event, check = true) {
 
     const text = check ? 'Marcar como lida esta' : 'Excluir'
     const text2 = check ? 'Tem certeza que você deseja marcar como lida' : 'Tem certeza que você deseja excluir'
-
     const slug = check ? 'check' : 'delete'
-
     const roomId = document.querySelector('#room-id').dataset.id
+    const questionId = event.target.dataset.id
 
     const form = document.querySelector('.modal form')
-    form.setAttribute('action', `/room/${roomId}/:question/${slug}`)
+    form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML = `${text} pergunta`
     modalDescription.innerHTML = `${text2} esta pergunta?`
