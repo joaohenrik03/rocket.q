e0800270c02b095fa33a5cccf53eec71b64bed41
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-31 00:02:38 -0300

    Create database and updates

--- src/db/config.js
+++ src/db/config.js
@@ -1,2 +1,8 @@
-const sqlite3 = require('sqlite3')
-const sqlite3 = require('sqlite3')
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite")
+
+module.exports = () => 
+    open({
+        filename: './src/db/myapp.sqlite',
+        driver: sqlite3.Database,
+    });
