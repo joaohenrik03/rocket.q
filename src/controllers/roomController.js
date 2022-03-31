cf913bd075b7b8ca76836732e03d845a1f6deda7
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-31 00:07:21 -0300

    Update roomController.js

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,6 +1,10 @@
 module.exports = {
     create(req, res) {
-        let roomId = 123456
+        let roomId = 0
+
+        for (var i = 0; i < 6; i ++) {
+            roomId += Math.floor(Math.random() * 10).toString()
+        }
 
         res.redirect(`/room/${roomId}`)
     }
