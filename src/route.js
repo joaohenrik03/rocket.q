3d8c9fd158e9e8a86616fd63cbd0648b91a45ece
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-03-31 20:43:39 -0300

    Update

--- src/route.js
+++ src/route.js
@@ -5,10 +5,12 @@ const roomController = require('./controllers/roomController')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
-route.get('/room/:room', roomController.open)
 route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
 
-route.post('/question/:room/:question/:action', questionController.index)
+route.get('/room/:room', roomController.open)
 route.post('/create-room', roomController.create)
+
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:room/:question/:action', questionController.index)
 
 module.exports = route
